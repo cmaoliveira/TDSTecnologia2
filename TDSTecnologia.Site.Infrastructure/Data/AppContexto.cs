6d9207722d1da672936a9627d5c70233719e2264
Cláudia Moreira Arrais de Oliveira <claudiaoliveira@controladoria.mt.gov.br>
2019-10-30 16:37:38 -0400

    Adicionar arquivos de projeto.

---
+++ TDSTecnologia.Site.Infrastructure/Data/AppContexto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTecnologia.Site.Core.Dominio;
+using TDSTecnologia.Site.Core.Entities;
+
+namespace TDSTecnologia.Site.Infrastructure.Data
+{
+    public class AppContexto : DbContext
+    {
+        public AppContexto(DbContextOptions<AppContexto> opcoes) : base(opcoes)
+        {
+        }
+
+        public DbSet<Curso> CursoDao { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+            .Entity<Curso>()
+            .Property(c => c.Turno)
+            .HasConversion(
+            v => v.ToString(),
+            v => (DomTurno)Enum.Parse(typeof(DomTurno), v));
+        }
+    }
+}
